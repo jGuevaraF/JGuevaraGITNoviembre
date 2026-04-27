c2d7fe251eb067df67b16b3d163cbad2550dc206
JorgeGF <jguevaraflores3@gmail.com>
2026-04-27 12:22:36 -0600

    Crear clase Usuario en la capa BL

---
+++ BL/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+        private readonly JguevaraProgramacionNcapasFebreroContext _context;
+
+        public Usuario(JguevaraProgramacionNcapasFebreroContext context)
+        {
+            _context = context;
+        }
+    }
+}
